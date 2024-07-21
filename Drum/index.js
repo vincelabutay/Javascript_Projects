976db47b3c7b2d7981fb4e9f29a3b53a857904b0
vincelabutay <vincelabutay@gmail.com>
2024-07-21 22:13:31 +0800

    drum

---
+++ Drum/index.js
@@ -0,0 +1,9 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+  function handleClick() {
+    alert("I got clicked!");
+  }
+}
